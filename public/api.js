2224ffbc66262823b8e1159dd5e8eef423acd4fa
jord4810 <jord4810@stthomas.edu>
2026-05-16 19:43:25 -0500

    Updated Styling for dashboard, app.js file, dashboard.js file, dashboard.html file, and updates to api.js file (not fully working still)

--- public/api.js
+++ public/api.js
@@ -54,3 +54,24 @@ async function deleteExercise(id) {
         console.error("Failed to delete exercise:", error);
     }
 }
+
+async function updateExercise(id, exerciseData) {
+    try {
+        const response = await fetch(`/api/exercises/${id}`, {
+            method: "PUT",
+            headers: getHeaders(),
+            body: JSON.stringify(exerciseData)
+        });
+        return await response.json();
+    } catch (error) {
+        console.error("Failed to update exercise:", error);
+    }
+}
+
+// Export the 4 methods so that we can use them in the dashboard.js
+export const api = {
+    getExercises,
+    createExercise,
+    deleteExercise,
+    updateExercise
+  };
